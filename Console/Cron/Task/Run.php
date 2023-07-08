d951c8bc7752c66f8bc8fde4acde891f7c4d5e6d
秋枫雁飞 <1714255949@qq.com>
2023-07-08 11:34:40 +0800

    异步任务优化

--- Console/Cron/Task/Run.php
+++ Console/Cron/Task/Run.php
@@ -15,6 +15,7 @@ namespace Weline\Cron\Console\Cron\Task;
 
 use Aiweline\KteInventory\Model\SiteCronTask;
 use Cron\CronExpression;
+use Weline\Cron\CronTaskInterface;
 use Weline\Cron\Helper\CronStatus;
 use Weline\Cron\Model\CronTask;
 use Weline\Framework\App\Env;
@@ -29,12 +30,15 @@ class Run implements CommandInterface
      * @var \Weline\Cron\Model\CronTask
      */
     private CronTask $cronTask;
+    private Printing $printing;
 
     public function __construct(
-        CronTask $cronTask
+        CronTask $cronTask,
+        Printing $printing
     )
     {
         $this->cronTask = $cronTask;
+        $this->printing = $printing;
     }
 
     /**
@@ -43,9 +47,10 @@ class Run implements CommandInterface
     public function execute(array $args = [], array $data = [])
     {
         array_shift($args);
-        $force = is_int(array_search('-f', $args));
+        $force   = is_int(array_search('-f', $args)) || is_int(array_search('-force', $args));
+        $process = is_int(array_search('-p', $args)) || is_int(array_search('-process', $args));
         foreach ($args as $key => $arg) {
-            if ($arg == '-f') {
+            if ($arg == '-f' || $arg == '-force' || $arg == '-p' || $arg == '-process') {
                 unset($args[$key]);
             }
         }
@@ -57,6 +62,29 @@ class Run implements CommandInterface
                 die;
             }
         }
+        # 如果给定的任务是单个任务，说明是具体要执行的任务
+        if ($process && count($task_names) == 1) {
+            /**@var CronTask $task */
+            $task = $this->cronTask->where($this->cronTask::fields_EXECUTE_NAME, array_shift($task_names))->find()->fetch();
+            if (!$task->getId()) {
+                ObjectManager::getInstance(Printing::class)->error(__('请指执行的任务不存在！'));
+                exit;
+            }
+            $class = $task->getData(CronTask::fields_CLASS);
+            /**@var CronTaskInterface $instance */
+            $instance = ObjectManager::getInstance($class);
+            $instance->execute();
+            $task->setData($task::fields_RUN_TIMES, (int)$task->getData($task::fields_RUN_TIMES) + 1);
+            # 设置程序运行数据
+            $task->setData($task::fields_BLOCK_TIME, 0);
+            # 解锁
+            $task->setData($task::fields_STATUS, CronStatus::SUCCESS->value);
+            $task_end_time = microtime(true) - ($task->getData(CronTask::fields_RUN_TIME));
+            $task->setData($task::fields_RUNTIME, $task_end_time);
+            # 运行完毕将进程ID设置为0
+            $task->setData($task::fields_PID, 0);
+            exit;
+        }
         # 读取给定的任务
         if ($task_names) {
             foreach ($task_names as $taskName) {
@@ -64,59 +92,69 @@ class Run implements CommandInterface
             }
         }
 
-        $tasks = $this->cronTask->select()->fetch()->getItems();
+        $pageSize = 1;
+        $this->cronTask->pagination(1, $pageSize)->select()->fetch();
+        # 分页读取任务
+        $taskTotal = (int)$this->cronTask->pagination['totalSize'];
+        $taskPages = (int)$this->cronTask->pagination['lastPage'];
         /**@var CronTask $taskModel */
         $descriptorspec = array(
             0 => array('pipe', 'r'),   // 子进程将从此管道读取stdin
             1 => array('pipe', 'w'),   // 子进程将向此管道写入stdout
             2 => array('pipe', 'w')    // 子进程将向此管道写入stderr
         );
-        foreach ($tasks as $taskModel) {
-            $task_start_time = microtime(true);
-            $task_run_date   = date('Y-m-d H:i:s');
-            # 上锁
-            $cron = CronExpression::factory($taskModel->getData('cron_time'));
-            if ($force || $cron->isDue($task_run_date)) {
-                if ($force || $taskModel->getData($taskModel::fields_STATUS) !== CronStatus::BLOCK->value) {
-                    # 设置程序运行数据
-                    # 上锁
-                    $taskModel->setData($taskModel::fields_STATUS, CronStatus::BLOCK->value);
-                    $taskModel->setData($taskModel::fields_RUN_TIME, $task_start_time);
-                    $taskModel->setData($taskModel::fields_RUN_DATE, $task_run_date);
-                    $taskModel->save(true);
-                    /**@var \Weline\Cron\CronTaskInterface $task */
-                    $task = ObjectManager::getInstance($taskModel->getData('class'));
-                    # 创建异步程序
-                    $command = 'cd ' . BP . ' && ' . PHP_BINARY . ' bin/m cron:task:run ' . $task->execute_name();
-                    $process = proc_open($command, $descriptorspec, $pipes);
-                    if (is_resource($process)) {
-                        $status = proc_get_status($process);
-                        $pid    = $status['pid'];
-                        # 记录PID
-                        $taskModel->setData($taskModel::fields_PID, $pid)
-                                  ->save();
-                        // 命令已成功作为异步进程执行
-                        // 关闭不需要的管道
-                        fclose($pipes[0]);
-                        // 读取子进程的输出（如果需要）
-                        $output = stream_get_contents($pipes[1]);
-                        // 关闭剩余的管道
-                        fclose($pipes[1]);
-                        fclose($pipes[2]);
-                        // 等待进程结束（如果需要）
-                        $exitCode = proc_close($process);
-                        // 根据需要处理输出或退出码
-                        // 继续执行其他任务或退出
-                    }else{
-                        $taskModel->setData($taskModel::fields_RUNTIME_ERROR, __('进程创建失败！请检查进程状态！'));
-                    }
-                } else {
-                    # 设置程序运行数据
-                    if ($run_time = $taskModel->getData($taskModel::fields_RUN_TIME)) {
-                        $taskModel->setData(
-                            $taskModel::fields_BLOCK_TIME,
-                            $task_start_time - $run_time
-                        );
+        foreach (range(1, $taskPages) as $current_page) {
+            $offset = ($current_page - 1) * $pageSize;
+            $currentTotal = $offset + $pageSize;
+            echo PHP_EOL;
+            CronStatus::displayProgressBar(__('任务进度：页(%1=>%2)/目(%3/%4)', [$taskPages,$current_page,$taskTotal,$currentTotal]), $currentTotal,
+                                           $taskTotal,false);
+            $tasks = $this->cronTask->limit($pageSize, $offset)
+                                    ->select()
+                                    ->fetch()
+                                    ->getItems();
+            # 进程信息管理
+            $processes = [];
+            $pipes     = [];
+            foreach ($tasks as $key => $taskModel) {
+                $task_start_time = microtime(true);
+                $task_run_date   = date('Y-m-d H:i:s');
+                $run_time        = $taskModel->getData($taskModel::fields_RUN_TIME) ?? 0;
+                # 上锁
+                $cron = CronExpression::factory($taskModel->getData('cron_time'));
+                if ($force || $cron->isDue($task_run_date)) {
+                    if ($force || $taskModel->getData($taskModel::fields_STATUS) !== CronStatus::BLOCK->value) {
+                        # 设置程序运行数据
+                        # 上锁
+                        $taskModel->setData($taskModel::fields_STATUS, CronStatus::BLOCK->value);
+                        $taskModel->setData($taskModel::fields_RUN_TIME, $task_start_time);
+                        $taskModel->setData($taskModel::fields_RUN_DATE, $task_run_date);
+                        $taskModel->save(true);
+                        /**@var \Weline\Cron\CronTaskInterface $task */
+                        $task = ObjectManager::getInstance($taskModel->getData('class'));
+                        # 创建异步程序
+                        $command = 'cd ' . BP . ' && ' . PHP_BINARY . ' bin/m cron:task:run -process ' . $task->execute_name() . ($force ? ' -force' : '');
+                        $process = proc_open($command, $descriptorspec, $procPipes);
+                        # 进程保存到进程数组
+                        $processes[$key] = $process;
+                        # 设置进程非阻塞
+                        stream_set_blocking($procPipes[1], false);
+                        $pipes[$key] = $procPipes;
+                        if (is_resource($process)) {
+                            $status = proc_get_status($process);
+                            $pid    = $status['pid'];
+                            # 记录PID
+                            $taskModel->setData($taskModel::fields_PID, $pid)
+                                      ->save();
+                        } else {
+                            $taskModel->setData($taskModel::fields_RUNTIME_ERROR, __('进程创建失败！请检查进程状态！'))
+                                      ->save();
+                        }
+                    } else {
+                        # 到了程序下次运行的时间，但是程序仍然处于block阻塞状态，设置程序运行阻塞数据
+                        if ($run_time) {
+                            $taskModel->setData($taskModel::fields_BLOCK_TIME, $task_start_time - $run_time);
+                        }
                         if ($block_time = $taskModel->getData($taskModel::fields_BLOCK_TIME)) {
                             if ($block_time > ($taskModel->getData($taskModel::fields_BLOCK_UNLOCK_TIMEOUT) * 60)) {
                                 $taskModel->setData($taskModel::fields_BLOCK_TIMES, (int)$taskModel->getData($taskModel::fields_BLOCK_TIMES) + 1);
@@ -126,32 +164,65 @@ class Run implements CommandInterface
                             }
                         }
                     }
+                } else {
+                    $taskModel->setData($taskModel::fields_STATUS, CronStatus::PENDING->value);
                 }
-            } else {
-                $taskModel->setData($taskModel::fields_STATUS, CronStatus::PENDING->value);
+                # 如果有进程PID，检测是否运行结束
+                if ($pid = $taskModel->getData($taskModel::fields_PID)) {
+                    $task_end_time = microtime(true) - $task_start_time;
+                    $isRunning     = posix_kill($pid, 0);
+                    if ($isRunning) {
+                        $taskModel->setData($taskModel::fields_BLOCK_TIME, $task_start_time - $run_time);
+                        $taskModel->save();
+                        continue;
+                    } elseif ($taskModel->getData($taskModel::fields_STATUS) !== CronStatus::SUCCESS->value) {
+                        $taskModel->setData($taskModel::fields_RUN_TIMES, (int)$taskModel->getData($taskModel::fields_RUN_TIMES) + 1);
+                        # 设置程序运行数据
+                        $taskModel->setData($taskModel::fields_BLOCK_TIME, 0);
+                        # 解锁
+                        $taskModel->setData($taskModel::fields_STATUS, CronStatus::SUCCESS->value);
+                        $taskModel->setData($taskModel::fields_RUNTIME, $task_end_time);
+                        # 运行完毕将进程ID设置为0
+                        $taskModel->setData($taskModel::fields_PID, 0);
+                        $taskModel->save();
+                    }
+                }
+                # 设置程序运行数据
+                $taskModel->setData($taskModel::fields_NEXT_RUN_DATE, $cron->getNextRunDate()->format('Y-m-d H:i:s'));
+                $taskModel->setData($taskModel::fields_MAX_NEXT_RUN_DATE, $cron->getNextRunDate('now', 3)->format('Y-m-d H:i:s'));
+                $taskModel->setData($taskModel::fields_PRE_RUN_DATE, $cron->getPreviousRunDate()->format('Y-m-d H:i:s'));
+                $taskModel->save(true);
             }
-            # 如果有进程PID，检测是否运行结束
-            if($pid=$taskModel->getData($taskModel::fields_PID)){
-                $task_end_time = microtime(true) - $task_start_time;
-                $isRunning = posix_kill($pid, 0);
-                if ($isRunning) {
-                    $taskModel->setData($taskModel::fields_BLOCK_TIME, $task_start_time - $run_time);
-                } elseif($taskModel->getData($taskModel::fields_STATUS) !== CronStatus::SUCCESS->value){
-                    $taskModel->setData($taskModel::fields_RUN_TIMES, (int)$taskModel->getData($taskModel::fields_RUN_TIMES) + 1);
+
+            # 循环检查各进程，直到所有子进程结束
+            while (array_filter($processes, function ($proc) { return proc_get_status($proc)['running']; })) {
+                foreach ($tasks as $i => $task) {
+                    # 如果有对应进程,读取所有可读取的输出（缓冲未读输出）
+                    if (!empty($pipes[$i])) {
+                        $str = fread($pipes[$i][1], 1024);
+                        if ($str) {
+                            echo $str;
+                        }
+                    }
+                }
+            }
+            # 关闭所有管道和进程
+            foreach ($tasks as $i => $task) {
+                if (!empty($pipes[$i])) {
+                    fclose($pipes[$i][1]);
+                    proc_close($processes[$i]);
+                    $task->setData($task::fields_RUN_TIMES, (int)$task->getData($task::fields_RUN_TIMES) + 1);
                     # 设置程序运行数据
-                    $taskModel->setData($taskModel::fields_BLOCK_TIME, 0);
+                    $task->setData($task::fields_BLOCK_TIME, 0);
                     # 解锁
-                    $taskModel->setData($taskModel::fields_STATUS, CronStatus::SUCCESS->value);
-                    $taskModel->setData($taskModel::fields_RUNTIME, $task_end_time);
+                    $task->setData($task::fields_STATUS, CronStatus::SUCCESS->value);
+                    $task_end_time = microtime(true) - $task_start_time;
+                    $task->setData($task::fields_RUNTIME, $task_end_time);
                     # 运行完毕将进程ID设置为0
-                    $taskModel->setData($taskModel::fields_PID, 0);
+                    $task->setData($task::fields_PID, 0);
+                    $task->save();
                 }
             }
-            # 设置程序运行数据
-            $taskModel->setData($taskModel::fields_NEXT_RUN_DATE, $cron->getNextRunDate()->format('Y-m-d H:i:s'));
-            $taskModel->setData($taskModel::fields_MAX_NEXT_RUN_DATE, $cron->getNextRunDate('now', 3)->format('Y-m-d H:i:s'));
-            $taskModel->setData($taskModel::fields_PRE_RUN_DATE, $cron->getPreviousRunDate()->format('Y-m-d H:i:s'));
-            $taskModel->save(true);
         }
     }
 
--- Helper/CronStatus.php
+++ Helper/CronStatus.php
@@ -20,4 +20,20 @@ enum CronStatus: string
     case BLOCK = 'block';
     case FAIL = 'fail';
     case MISS = 'miss';
+
+    static function displayProgressBar(string $title, int $current, int $total, bool $scoall = true): void
+    {
+        if($current>$total){
+            $current = $total;
+        }
+        if ($scoall) echo PHP_EOL;
+        $percentage      = (int)round(($current / $total) * 100);
+        if(empty($percentage)){
+            $percentage = 1;
+        }
+        $progress        = '[' . str_repeat("\033[42m>\033[0m", $percentage) . str_repeat(' ', 100 - $percentage) . ']';
+        $coloredProgress = "\033[42m{$progress}\033[0m"; // 设置背景绿色
+        echo "\033[34m {$title}（{$total}\\{$current}）: \033[0m {$coloredProgress} \033[32m {$percentage}% \033[0m \r";
+        flush();
+    }
 }
