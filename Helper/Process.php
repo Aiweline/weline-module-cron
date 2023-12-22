606851b4833b152a47272fb2a918e5d7ba4fb0b3
秋枫雁飞 <1714255949@qq.com>
2023-12-22 01:31:56 +0000

    add Helper/Process.php.
    Signed-off-by: 秋枫雁飞 <1714255949@qq.com>

---
+++ Helper/Process.php
@@ -0,0 +1,74 @@
+<?php
+declare(strict_types=1);
+
+/*
+ * 本文件由 秋枫雁飞 编写，所有解释权归Aiweline所有。
+ * 作者：Administrator
+ * 邮箱：aiweline@qq.com
+ * 网址：aiweline.com
+ * 论坛：https://bbs.aiweline.com
+ * 日期：22/12/2023 09:26:37
+ */
+
+namespace Weline\Cron\Helper;
+
+class Process
+{
+
+    static public function killPid(int $pid)
+    {
+        if (strtoupper(substr(PHP_OS, 0, 3)) !== 'WIN') {
+            exec("kill $pid 2>/dev/null", $output, $exitCode);
+            return $exitCode === 0;
+        } else {
+            exec("taskkill /F /PID $pid 2>NUL", $output, $exitCode);
+            return $exitCode === 0;
+        }
+    }
+
+    static public function isProcessRunning(int $pid)
+    {
+        if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
+            $output = [];
+            exec("tasklist /FI \"PID eq $pid\" 2>NUL", $output);
+            foreach ($output as $line) {
+                if (strpos($line, " $pid ") !== false) {
+                    return true;
+                }
+            }
+        } else {
+            $output = [];
+            exec("ps -p $pid", $output);
+            return count($output) > 1;
+        }
+        return false;
+    }
+
+    static public function getProcessOutput(int $pid): string|false
+    {
+        $output = false;
+        if (!$this->isProcessRunning($pid)) {
+            return $output;
+        }
+        if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
+            // Windows 上的实现
+            $command = "wmic process get ProcessId,CommandLine | findstr $pid";
+            $output  = shell_exec($command);
+        } else {
+            // Linux 上的实现
+            if (file_exists("/proc/$pid")) {
+                $descriptors = [
+                    1 => ['pipe', 'w'],
+                ];
+                $process = proc_open("cat /proc/$pid/fd/1", $descriptors, $pipes);
+                if (is_resource($process)) {
+                    $output = stream_get_contents($pipes[1]);
+                    fclose($pipes[1]);
+                    proc_close($process);
+                    return $output;
+                }
+            }
+        }
+        return $output;
+    }
+}
